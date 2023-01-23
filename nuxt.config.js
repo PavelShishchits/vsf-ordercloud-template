876135f69cd15fe781417b84128191865379364f
pavel s <p.shischits@ext.kuehlhaus.com>
2023-01-23 18:08:19 +0300

    Revert "chore: add pathe to nuxt build transpile list"
    This reverts commit 583d7c9fa82dc5e7fac9bc2271c35708c254f581.

--- nuxt.config.js
+++ nuxt.config.js
@@ -118,8 +118,7 @@ export default {
     transpile: [
       'vee-validate/dist/rules',
       'storefront-ui',
-      '@glidejs/glide',
-      'pathe'
+      '@glidejs/glide'
     ],
     plugins: [
       new webpack.DefinePlugin({
